79a2d94e006ff680a2ecfbb7f4dd92cf6bf89ff1
moayidalsubhi <moayidalsubhi@gmail.com>
2018-06-01 03:43:47 +0400

    added files

--- js/qrscan.js
+++ js/qrscan.js
@@ -1,8 +1,64 @@
+
+        var ar = true;
+        var en = false;
+        var temp="";
+
+        function setar(){
+
+
+
+            ar = true;
+            en = false;
+
+            
+        if(temp != ""){
+          document.getElementById("audio_player").src = "arvoice/" + temp + ".mp3";
+        }
+
+        }
+
+        function seten(){
+
+          if(temp != ""){
+            document.getElementById("audio_player").src = "envoice/" + temp + ".mp3";
+          }
+            ar = false;
+            en = true;
+        }
+
+
+
+   
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 scanner.addListener('scan', function (content) {
   console.log(content);
+  temp = content;
+  if(ar == true){
+
+    document.getElementById("audio_player").src = "arvoice/" + content + ".mp3";
+
+  }else{
+
+    document.getElementById("audio_player").src = "envoice/" + content + ".mp3";
+
+  }
 });
 Instascan.Camera.getCameras().then(function (cameras) {
+//open back camera///
+  const constraints = {
+    video: true,
+    audio: false
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+      video.srcObject = stream;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+    ///////////////////////////////////
+
   if (cameras.length > 0) {
     scanner.start(cameras[0]);
   } else {

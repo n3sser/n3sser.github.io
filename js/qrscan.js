9eb027c63c2c0273acc11c55b67619dd8ea4e6f2
moayidalsubhi <moayidalsubhi@gmail.com>
2018-06-01 04:17:16 +0400

    back camera changes

--- js/qrscan.js
+++ js/qrscan.js
@@ -2,49 +2,32 @@
         var ar = true;
         var en = false;
         var temp="";
-
         function setar(){
-
-
-
             ar = true;
-            en = false;
-
-            
+            en = false;     
         if(temp != ""){
           document.getElementById("audio_player").src = "arvoice/" + temp + ".mp3";
         }
-
         }
-
         function seten(){
-
           if(temp != ""){
             document.getElementById("audio_player").src = "envoice/" + temp + ".mp3";
           }
             ar = false;
             en = true;
         }
-
-
-
-   
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 scanner.addListener('scan', function (content) {
   console.log(content);
   temp = content;
   if(ar == true){
-
     document.getElementById("audio_player").src = "arvoice/" + content + ".mp3";
-
-  }else{
-
+  }
+  else{
     document.getElementById("audio_player").src = "envoice/" + content + ".mp3";
-
   }
 });
 Instascan.Camera.getCameras().then(function (cameras) {
-
   if (cameras[1]) {
     scanner.start(cameras[1]);
   }

9ab38ec8d9e69ba7285df497b9308a7b275de16b
moayidalsubhi <moayidalsubhi@gmail.com>
2018-06-01 04:21:36 +0400

    back camera update

--- js/qrscan.js
+++ js/qrscan.js
@@ -28,6 +28,21 @@ scanner.addListener('scan', function (content) {
   }
 });
 Instascan.Camera.getCameras().then(function (cameras) {
+
+  if (cameras.length > 0) {
+    var selectedCam = cameras[0];
+    $.each(cameras, (i, c) => {
+        if (c.name.indexOf('back') != -1) {
+            selectedCam = c;
+            return false;
+        }
+    });
+
+    scanner.start(selectedCam);
+} else {
+    console.error('No cameras found.');
+}
+/*
   if (cameras[1]) {
     scanner.start(cameras[1]);
   }
@@ -37,6 +52,7 @@ Instascan.Camera.getCameras().then(function (cameras) {
   else {
     console.error('No cameras found.');
   }
+  */
 }).catch(function (e) {
   console.error(e);
 });

4f324cbe11f67b74754555ac60283bf8251ae297
n3sser <nalsubhi996@gmail.com>
2018-06-01 13:46:28 +0400

    yy

--- js/qrscan.js
+++ js/qrscan.js
@@ -25,8 +25,63 @@
             ar = false;
             en = true;
         }
+var videoElement = document.querySelector('video');
+var audioSelect = document.querySelector('select#audioSource');
+var videoSelect = document.querySelector('select#videoSource');
 
+navigator.mediaDevices.enumerateDevices()
+  .then(gotDevices).then(getStream).catch(handleError);
 
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+        function gotDevices(deviceInfos) {
+          for (var i = 0; i !== deviceInfos.length; ++i) {
+            var deviceInfo = deviceInfos[i];
+            var option = document.createElement('option');
+            option.value = deviceInfo.deviceId;
+            if (deviceInfo.kind === 'audioinput') {
+              option.text = deviceInfo.label ||
+                'microphone ' + (audioSelect.length + 1);
+              audioSelect.appendChild(option);
+            } else if (deviceInfo.kind === 'videoinput') {
+              option.text = deviceInfo.label || 'camera ' +
+                (videoSelect.length + 1);
+              videoSelect.appendChild(option);
+            } else {
+              console.log('Found one other kind of source/device: ', deviceInfo);
+            }
+          }
+        }
+
+        function getStream() {
+          if (window.stream) {
+            window.stream.getTracks().forEach(function(track) {
+              track.stop();
+            });
+          }
+        
+          var constraints = {
+            audio: {
+              deviceId: {exact: audioSelect.value}
+            },
+            video: {
+              deviceId: {exact: videoSelect.value}
+            }
+          };
+        
+          navigator.mediaDevices.getUserMedia(constraints).
+            then(gotStream).catch(handleError);
+        }
+        function gotStream(stream) {
+          window.stream = stream; // make stream available to console
+          videoElement.srcObject = stream;
+        }
+        
+        function handleError(error) {
+          console.log('Error: ', error);
+        }
+        
    
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview')});
 scanner.addListener('scan', function (content) {
@@ -47,49 +102,24 @@ scanner.addListener('scan', function (content) {
   
 });
 Instascan.Camera.getCameras().then(function (cameras) {
-//open back camera///
-  const constraints = {
-    video: true,
-    audio: false
-  };
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(stream => {
-      video.srcObject = stream;
-    })
-    .catch(error => {
-      console.error(error);///kjlkj
+
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
     });
-    ///////////////////////////////////
-
-  if (cameras.length > 0) {
-
-
- 
-    var selectedCam = cameras[0];
-    /*scanner.start(cameras[0]);
-    $.each(cameras, (i, c) => {
-        if (c.name.indexOf('back') != -1) {
-            selectedCam = c;
-            return false;
-        }
-    });*/
-
-
-    if (cameras.length > 0) {
-      var selectedCam = cameras[0];
-      $.each(cameras, (i, c) => {
-          if (c.name.indexOf('back') != -1) {
-              selectedCam = c;
-              return false;
-          }
-      });
-  
-      scanner.start(selectedCam);
-
-  } else {
-    console.error('No cameras found.');
   }
-}).catch(function (e) {
-  console.error(e);
+//open back camera///
+  const constraints  = {
+    audio: {
+      deviceId: {exact: audioSelect.value}
+    },
+    video: {
+      deviceId: {exact: videoSelect.value}
+    }
+  };
+  navigator.mediaDevices.getUserMedia(constraints).
+  then(gotStream).catch(handleError);
+
+  scanner.start(cameras[0]);
+    ///////////////////////////////////
 });

6c22de3a8370b408fe13ae85ff6a64f963f6dddc
n3sser <nalsubhi996@gmail.com>
2018-06-01 07:50:22 +0400

    nn

--- js/qrscan.js
+++ js/qrscan.js
@@ -64,7 +64,7 @@ Instascan.Camera.getCameras().then(function (cameras) {
 
  
   
-      if(cameras[1]){
+      if(cameras.length > 1){
         scanner.start(cameras[1]);
       }else{
         scanner.start(cameras[0]);
